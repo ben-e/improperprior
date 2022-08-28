7ce8962a4a69db85b95a8342681c897f5bacf20f
Ben E <benewing1992@gmail.com>
2022-08-28 11:33:02 -0700

    Quarto migration post

--- meta-R/templates/ggplot2-template.R
+++ meta-R/templates/ggplot2-template.R
@@ -19,7 +19,7 @@ scale_colour_ip <- function( ...) {
 scale_color_ip <- scale_colour_ip
 
 scale_fill_ip <- function(...) {
-  discrete_scale("colour", "ip", ip_pal(), ...)
+  discrete_scale("fill", "ip", ip_pal(), ...)
 }
 
 # Get palette entries with roles as a data frame
@@ -33,6 +33,9 @@ palette_df <- data.frame(
 primary_rgb <- col2rgb(palette_df$primary)/255
 primary_lite <- rgb(primary_rgb[1], primary_rgb[2], primary_rgb[3], 0.1)
 
+grid_color <- col2rgb(palette_df$text_color)/255
+grid_color <- rgb(grid_color[1], grid_color[2], grid_color[3], 0.3)
+
 theme_ip <- function(base_size = 12, base_family = "Montserrat") {
   # is it better to set the background to transparent, or to match the theme?
   theme_minimal(base_size = base_size, base_family = base_family) +
@@ -40,6 +43,6 @@ theme_ip <- function(base_size = 12, base_family = "Montserrat") {
           legend.position = "bottom",
           panel.background = element_rect(fill = primary_lite, colour = NA),
           plot.background = element_rect(fill = "transparent", colour = NA),
-          panel.grid.major = element_line(colour = palette_df$text_color, size = 1/3),
-          panel.grid.minor = element_line(colour = palette_df$text_color, size = 1/6))
+          panel.grid.major = element_line(colour = grid_color, size = 1/3),
+          panel.grid.minor = element_line(colour = grid_color, size = 1/6))
 }
